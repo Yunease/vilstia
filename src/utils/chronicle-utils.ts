8e2a92bbbfbd65ac3791f0ea6f3c08ada235a0f2
yunease <2642076599@qq.com>
2026-05-08 15:20:44 +0800

    修改getchronicle函数，将年代记录的功能改为倒叙。

--- src/utils/chronicle-utils.ts
+++ src/utils/chronicle-utils.ts
@@ -5,6 +5,6 @@ export async function getChronicleEvents(): Promise<CollectionEntry<"chronicle">
 	return entries.sort((a, b) => {
 		const dateA = new Date(a.data.date);
 		const dateB = new Date(b.data.date);
-		return dateA > dateB ? -1 : 1;
+		return dateA > dateB ? 1 : -1;
 	});
 }
